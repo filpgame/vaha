3261928af3f8e2277e5fdc2aed83562da2ee3b40
csanterio@gmail.com <csanterio@gmail.com@ee5456b1-e66d-bef0-64c3-97a79d93c89e>
2014-01-30 14:31:42 +0000

    VAHA atual... xD

--- gerarPerguntasAleatorias.php
+++ gerarPerguntasAleatorias.php
@@ -9,23 +9,6 @@
     
     function gerar(){
         
-        $maiorID = "SELECT MAX(id_pergunta) FROM pergunta";
-        $menorID = "SELECT MIN(id_pergunta) FROM pergunta";
-        
-        $id_premiado = rand($menorID, $maiorID);
-        
-        $query = "SELECT pergunta FROM pergunta WHERE id_pergunta = $id_premiado";
-        
-        $resultado = mysqli_query($conn, $query);
-        
-        if($resultado){
-            /* Fazer algo aqui ... */
-        }else{
-            $id_premiado = rand($menorID, $maiorID);        
-            $query = "SELECT pergunta FROM pergunta WHERE id_pergunta = $id_premiado";
-        }
-        
-        
         
     }
 
