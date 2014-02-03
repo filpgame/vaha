c346cebd3b3d24c464e1e4ec87e106e98010e6ed
filpgame <filpgame@ee5456b1-e66d-bef0-64c3-97a79d93c89e>
2014-02-03 22:50:06 +0000

    Link dos objetos para incorporar na página EstudeAqui

--- trunk/logado/index.php
+++ trunk/logado/index.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<!--
+placamae - https://sketchfab.com/show/cf088e43284248f6bdd4c0d3dc4fd18a
+processador - https://sketchfab.com/show/c588d83c3b1b4ad68e18f56e75f635ce
+SSd - https://sketchfab.com/show/b802bd65f93a49bf9e8826ed676a14e0
+HDD - https://sketchfab.com/show/1e699db0fade40eab161603387db918e
+
+-->
 <?php
 include("includes/info_logado.php");
 ?>
@@ -11,5 +18,6 @@ include("includes/info_logado.php");
         <?php
         echo "Bem vindo $nome $sobrenome !";
         ?>
+        <iframe frameborder="0" height="480" width="854" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" src="https://sketchfab.com/nmlkfe10?autostart=0&transparent=0&autospin=0&controls=1"></iframe> <p style="font-size: 13px; font-weight:normal; margin: 5px; color: #4a4a4a"><strong><a style="color: #1caad9;" href="http://sketchfab.com/show/5f5fe4d8b75f4f4e9fc9f13726b29adb">LG Nexus 5</a> from <a style="color: #1caad9;" href="http://sketchfab.com/adamselene">Ti</a> on <a style="color: #1caad9;" href="https://sketchfab.com">Sketchfab</a>.</strong></p>
     </body>
 </html>
