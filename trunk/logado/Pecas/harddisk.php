7854624992bfee9d49408c784d1c841a869fb445
filpgame <filpgame@ee5456b1-e66d-bef0-64c3-97a79d93c89e>
2014-02-19 05:33:10 +0000

    Audio inserido no harddisk.php e no cooler.php (Funcionando)

--- trunk/logado/Pecas/harddisk.php
+++ trunk/logado/Pecas/harddisk.php
@@ -15,7 +15,14 @@ $resultado = mysqli_query($conn, $query);
     <body>
         <div id="conteudo">
             <iframe frameborder="0" height="320" width="400" allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" src="https://sketchfab.com/nm5lkh20ba?autostart=1&transparent=0&autospin=1&controls=1"></iframe>
-
+            <div style="display: inline-block;">
+                <p style="font-weight: bold; font-size: small;">Clique e escute a explicação<br> sobre o componente!</p>
+                <audio controls style="margin-bottom: 140px;width: 217px;">
+                    <source src="audio/harddisk.3ga" type="audio/ogg">
+                    <source src="audio/harddisk.3ga" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+            </div>
             <?php while ($linha = mysqli_fetch_array($resultado, MYSQLI_ASSOC)): ?>
                 <div id="explicacao-3d"> <?php echo $linha['desc']; ?> </div>
             <?php endwhile; ?>
